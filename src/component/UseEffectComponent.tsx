cc48cefb380b8be1bb856ef7244f816dbf770c09
ddeveza <ddeveza@ymail.com>
2022-06-03 10:10:32 +0800

    completed useeffect

--- src/component/UseEffectComponent.tsx
+++ src/component/UseEffectComponent.tsx
@@ -1,16 +1,16 @@
 import {useEffect, useState} from "react";
 
 const UseEffectComponent = () => {
-    const [time, setTime] = useState<Date>();
+    const [time, setTime] = useState(new Date());
 
     useEffect(() => {
         const timer = setTimeout(() => setTime(new Date()), 1000);
         return () => {
             clearTimeout(timer);
         };
-    }, []);
+    }, [time]);
 
-    return <>{time}</>;
+    return <div className='font-bold'>{time.toLocaleTimeString()}</div>;
 };
 
 export default UseEffectComponent;
